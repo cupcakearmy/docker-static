9f7b912536230697d8cae47a997150d9001530a2
cupcakearmy <hi@nicco.io>
2022-11-01 22:05:49 +0100

    try with newlinw

--- tags/index.js
+++ tags/index.js
@@ -24,7 +24,7 @@ function convert(version, additional = []) {
   return {
     version,
     // https://github.com/docker/metadata-action#typeraw
-    tags: [version, ...additional].join(','),
+    tags: [version, ...additional].join('\n'),
   }
 }
 
